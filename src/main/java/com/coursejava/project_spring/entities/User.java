e7bee108d3f0160c96bf638220debead9e884a4f
Gabriel Apolinário Fabrício <gabriel.fabricio08@outlook.com>
2024-08-13 17:55:54 -0300

    Conectando com banco de dados H2 e JPA

--- src/main/java/com/coursejava/project_spring/entities/User.java
+++ src/main/java/com/coursejava/project_spring/entities/User.java
@@ -1,9 +1,15 @@
 package com.coursejava.project_spring.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
-
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
