2cd31c0cff334576ae83a52bd7994d56b4e44287
Gabriel Apolinário Fabrício <gabriel.fabricio08@outlook.com>
2024-08-16 10:10:03 -0300

    criando associação Product-OrderItem um para muitos

--- src/main/java/com/coursejava/project_spring/entities/Product.java
+++ src/main/java/com/coursejava/project_spring/entities/Product.java
@@ -1,5 +1,6 @@
 package com.coursejava.project_spring.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -23,6 +24,8 @@ public class Product implements Serializable {
     inverseJoinColumns = @JoinColumn(name = "category_id")
     )
     private Set<Category> categories = new HashSet<>();
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product() {
     }
@@ -76,6 +79,14 @@ public class Product implements Serializable {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+    @JsonIgnore
+    public Set<Order> getOrders() {
+        Set<Order> set = new HashSet<>();
+        for (OrderItem oi:items) {
+            set.add(oi.getOrder());
+        }
+        return set;
     }
 
     @Override
