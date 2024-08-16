b697eaf742510628ce0b9db15d8411120ac5bb09
Gabriel Apolinário Fabrício <gabriel.fabricio08@outlook.com>
2024-08-16 12:35:30 -0300

    criando tratamento de exceção método findById

--- src/main/java/com/coursejava/project_spring/services/UserService.java
+++ src/main/java/com/coursejava/project_spring/services/UserService.java
@@ -2,6 +2,7 @@ package com.coursejava.project_spring.services;
 
 import com.coursejava.project_spring.entities.User;
 import com.coursejava.project_spring.repositories.UserRepository;
+import com.coursejava.project_spring.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> user = userRepository.findById(id);
-        return user.get();
+        return user.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User post(User user) {
---
+++ src/main/java/com/coursejava/project_spring/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.coursejava.project_spring.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found. Id " + id);
+    }
+}
