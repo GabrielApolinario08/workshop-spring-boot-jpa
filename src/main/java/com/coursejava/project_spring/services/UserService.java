697733a02c26b6da10961a833e65e40706378965
Gabriel Apolinário Fabrício <gabriel.fabricio08@outlook.com>
2024-08-16 11:34:44 -0300

    inserindo usuário método post

--- src/main/java/com/coursejava/project_spring/services/UserService.java
+++ src/main/java/com/coursejava/project_spring/services/UserService.java
@@ -3,6 +3,7 @@ package com.coursejava.project_spring.services;
 import com.coursejava.project_spring.entities.User;
 import com.coursejava.project_spring.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,5 +21,9 @@ public class UserService {
     public User findById(Long id) {
         Optional<User> user = userRepository.findById(id);
         return user.get();
+    }
+
+    public User post(User user) {
+        return userRepository.save(user);
     }
 }
